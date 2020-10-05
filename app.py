7f7bf61dda486f3dbfcea9912956810f625b7ae6
Stephan Hartung <stephan.hartung@de.ibm.com>
2020-10-05 11:11:22 +0200

    Added arguments processing, argument <name>

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "IKS-LAB: Hello beautiful world!"
+    name = request.args.get("name", "world")
+    return "IKS-LAB: Hello beautiful {escape(name)}!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
