e149b13974659e99c033a9decbfb7c7a2bbe515e
Stephan Hartung <stephan.hartung@de.ibm.com>
2020-10-05 11:27:24 +0200

    Bugfix

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, request, escape
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     name = request.args.get("name", "world")
-    return "IKS-LAB: Hello beautiful {escape(name)}!"
+    return f"IKS-LAB: Hello beautiful {escape(name)}!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
