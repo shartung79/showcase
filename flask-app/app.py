c6cf547c2f2e5c3eca5481d14fb8ffb84e0f6186
Stephan Hartung <stephan.hartung@de.ibm.com>
2020-10-09 10:50:44 +0200

    Update

--- flask-app/app.py
+++ flask-app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     name = request.args.get("name", "world")
-    return f"SHOWCASE-LAB: Hello beautiful {escape(name)}!"
+    return f"SHOWCASE-LAB: Hello perfect {escape(name)}!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8080)
